5028e2e5b9b48fb5f0d14813805917e21298e2d5
lucaoudejans <luca.oudejans@hva.nl>
2023-02-23 14:02:54 +0100

    run

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require (`express`)
+const app = express()
+let ejs = require('ejs')
+
+const port = 4000
+
+app.use(express.static('static'))
+
+express()
+.use(express.static('static'))
+.set('view.engine', 'ejs')
+.set('views', 'view')
+
+app.get('/', (reg, res) => {
+    res.send('<img src="/images/kat.jpeg" width="150">Hello world!')
+})
+
+app.get('/home/:user/', (req, res) => {
+    console.log(`Input from ${req.params.user}`)
+    res.send('<img src="/images/kat.jpeg" width="150"><h1>Hello World!' + req.params.user + '</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Hoi on port ${port}`)
+})
