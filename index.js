692d4b2d2c95b7e894d97c10ab1e156e629ed6c7
lucaoudejans <luca.oudejans@hva.nl>
2023-03-10 13:19:33 +0100

    fixed connection by putting env in the right map

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-// geheime bestanden in env
+// hidden in env
 require('dotenv').config()
 
-// express applicatie importeren
+// import express application 
 const express = require('express')
 const app = express()
 
-// express luistert naar de port. 4000 wordt weergegeven in de console
+// express listens to the port. 4000 shows in console
 const port = 4000
 
-// mongoDB connecten
+// connecting mongoDB connect
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const dbName = 'testdatab'
 
+// trying to fix error in terminal
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
     client.close();
   });
 
-// beide collections aanroepen
+// calling both collections 
 const db = client.db(dbName)
 const form = db.collection('form')
 const pets = db.collection('pets')
 
-// ejs view engine importeren
+// import ejs view engine 
 app.set('view engine', 'ejs')
 
-// ontleed form gegevens en kan deze nesten
+// parses form data and can nest it
 app.use(express.urlencoded({extended: true}))
 
-// middleware bedient de static bestanden. Alle verzoeken die beginnen met static worden 
-// geleverd met bestanden uit de directory ./static
+// middleware serves the static files. Alle requests that starts with /static will 
+// be delivered with files from the directory ./static
 app.use('/static/', express.static('./static'));
 
-// dynamische weergave wordt weergegeven
+// dynamic view is being viewed
 app.set('view engine', 'ejs')
-// wordt gekeken voor view templates in de view map
+// is looking for view templates in the view map
 app.set('views', 'view')
 
-// hieronder alle pages
+// all pages
 
-// het ejs bestand wordt aangeroepen en komt in html terug
+// calling ejs and returning in html
 app.get('/', function(req, res) {
     res.render('index.ejs');
   });
 
-// wanneer user een bron opvraagt wat niet bestaat, komt er een error melding 
+// when an user is calling an unknow file, an error occurs
 app.use((req, res) => {
   res.status(404).send("404, error")
 })
 
-// 4000 komt in de console om aan te geven dat de server luistert 
+// 4000 shows in the console to let know it works
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 });
